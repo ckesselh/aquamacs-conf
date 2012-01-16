4b4f354006ec9157c24bebf268e70c2f21311801
Christian Kesselheim <christian.kesselheim@kreios.lu>
2012-01-16 21:47:09 +0100

    Initial set of files as generated by Aquamacs on start-up

---
+++ Library/Preferences/Aquamacs Emacs/frame-positions.el
@@ -0,0 +1,8 @@
+;; Saved Frame Positions
+
+
+(if (fboundp (quote smart-frame-set-initial-frame-alist)) (smart-frame-set-initial-frame-alist (quote ((left . 4) (top . 22) (width . 178) (height . 41)))))
+
+
+
+(setq smart-frame-prior-positions (quote nil))
