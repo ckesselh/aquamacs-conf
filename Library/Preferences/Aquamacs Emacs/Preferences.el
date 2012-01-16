3501b20b2de2ca16b7858fcf4cc51576de883c7e
Christian Kesselheim <christian.kesselheim@kreios.lu>
2012-01-16 22:14:02 +0100

    Made sure CMD is bound to META and that the OS' clipboard is properly integrated, as well

--- Library/Preferences/Aquamacs Emacs/Preferences.el
+++ Library/Preferences/Aquamacs Emacs/Preferences.el
@@ -4,3 +4,11 @@
 ;; evaluating this file and print errors in the *Messags* buffer.
 ;; Use this file in place of ~/.emacs (which is loaded as well.)
 
+;; Make sure we can still type-in special characters (e.g. '[') when using Emacs on MacOSX
+(setq mac-option-key-is-meta nil)
+(setq mac-command-key-is-meta t)
+(setq mac-command-modifier 'meta)
+(setq mac-option-modifier nil)
+
+;; Merge the Emacs kill-ring with the OS' clipboard
+(setq x-select-enable-clipboard t)
