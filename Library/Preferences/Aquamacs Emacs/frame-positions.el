ce0a85cdf4bc9f3c464b200c89c93ef04c883f20
Christian Kesselheim <christian.kesselheim@kreios.lu>
2012-01-16 22:22:28 +0100

    Dropped some more auto-generated files

--- Library/Preferences/Aquamacs Emacs/frame-positions.el
+++
@@ -1,8 +0,0 @@
-;; Saved Frame Positions
-
-
-(if (fboundp (quote smart-frame-set-initial-frame-alist)) (smart-frame-set-initial-frame-alist (quote ((left . 4) (top . 22) (width . 178) (height . 41)))))
-
-
-
-(setq smart-frame-prior-positions (quote nil))
